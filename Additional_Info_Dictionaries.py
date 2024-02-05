55a0157aeb45788823772e716151dc6d93431c15
Doug Griep <dmgriep@gmail.com>
2024-02-04 21:05:14 -0600

    organizing by how items appear in game

--- Additional_Info_Dictionaries.py
+++ Additional_Info_Dictionaries.py
@@ -14,17 +14,22 @@ from WeaponEnums import (
     MeleeSecondary,
 )
 from AttachmentEnums import (
-    Optics,
     Muzzles,
-    Underbarrels,
-    RearGrips,
-    AmmoTypes,
-    Combs,
-    Stocks,
-    Lasers,
     Barrels,
+    Arms,
+    Lasers,
+    Optics,
+    Stocks,
+    Combs,
+    Guards,
+    Bolts,
+    RearGrips,
+    TriggerActions,
     Magazines,
-    Guards
+    Wires,
+    AmmoTypes,
+    Underbarrels,
+    CarryHandles,
 )
 from OperatorItemsEnums import (
     Vest,
@@ -37,53 +42,58 @@ from OperatorItemsEnums import (
 )
 
 aftermarket_parts = {
+    "Muzzles": [],
+    "Barrels": [],
+    "Arms": [],
+    "Lasers": [],
     "Optics": [
         Optics.KR_INTLAS_LSJ_3.value,
         Optics.CRONEN_INTLAS_MSP_12.value,
         Optics.INTLAS_CAS_14.value,
     ],
-    "Muzzles": [],
-    "Underbarrels": [],
-    "RearGrips": [],
-    "AmmoTypes": [],
-    "Combs": [],
     "Stocks": [],
-    "Lasers": [],
-    "Barrels": [],
+    "Combs": [],
+    "Guards": [],
+    "Bolts": [],
+    "RearGrips": [],
+    "TriggerActions": [],
     "Magazines": [],
-    "Guards": []
+    "Wires": [],
+    "AmmoTypes": [],
+    "Underbarrels": [],
+    "CarryHandles": [],
 }
 
 armory_unlocks = {
     "Weapons": {
         "AssaultRifles": [
             AssaultRifles.DG_56.value,
-            AssaultRifles.FR_556.value
+            AssaultRifles.FR_556.value,
         ],
         "BattleRifles": [
             BattleRifles.SIDEWINDER.value,
-            BattleRifles.MTZ_762.value
+            BattleRifles.MTZ_762.value,
         ],
         "SMGS": [
             SMGS.WSP_9.value,
             SMGS.RIVAL_9,
-            SMGS.STRIKER_9.value
+            SMGS.STRIKER_9.value,
         ],
         "Shotguns": [
-            Shotguns.RIVETER.value
+            Shotguns.RIVETER.value,
         ],
         "LMGS": [
             LMGS.HOLGER_26.value,
-            LMGS.TAQ_ERADICATOR.value
+            LMGS.TAQ_ERADICATOR.value,
         ],
         "MarksmanRifles": [
+            MarksmanRifles.MCW_68.value,
             MarksmanRifles.DM56.value,
             MarksmanRifles.MTZ_INTERCEPTOR.value,
-            MarksmanRifles.MCW_68.value
         ],
         "SniperRifles": [
             SniperRifles.LONGBOW.value,
-            SniperRifles.KV_INHIBITOR.value
+            SniperRifles.KV_INHIBITOR.value,
         ],
         "MeleePrimary": [],
         "Handguns": [
@@ -100,12 +110,12 @@ armory_unlocks = {
             Optics.CRONEN_INTLAS_MSP_12.value,
             Optics.INTLAS_CAS_14.value,
         ],
-        "Underbarrels": [
-            Underbarrels.JAK_PURIFIER.value,
-        ],
         "Stocks": [
             "MCW FULL AUTO CONVERTER",
             "WSP AKIMBO BRACE STOCK"
+        ],
+        "Underbarrels": [
+            Underbarrels.JAK_PURIFIER.value,
         ],
         "Kits": [
             "BROODMOTHER .45 KIT",
@@ -113,57 +123,56 @@ armory_unlocks = {
             "XRK IP-V2 CONVERSION KIT",
             "JAK RAVEN KIT"
         ],
-
     },
     "Perks": [
-        Gear.GHOST_TV_CAMO.value,
+        Vest.ASSASSIN_VEST.value,
+        Gloves.MARKSMAN_GLOVES.value,
         Gloves.ASSAULT_GLOVES.value,
+        Boots.COVERT_SNEAKERS.value,
+        Gear.THREAT_IDENTIFICATION_SYSTEM.value,
+        Gear.DATA_JACKER.value,
         Gear.SIGNAL_JAMMER.value,
         Gear.HIJACKED_IFF_STROBE.value,
-        Gear.THREAT_IDENTIFICATION_SYSTEM.value,
-        Boots.COVERT_SNEAKERS.value,
-        Gear.DATA_JACKER.value,
-        Gloves.MARKSMAN_GLOVES.value,
-        Vest.ASSASSIN_VEST.value,
+        Gear.GHOST_TV_CAMO.value,
     ],
     "Equipment": [
-        Lethal.SEMTEX.value,
         Lethal.PROXIMITY_MINE.value,
+        Lethal.DRILL_CHARGE.value,
+        Lethal.SEMTEX.value,
         Lethal.C4.value,
         Lethal.BREACHER_DRONE.value,
-        Lethal.DRILL_CHARGE.value,
         Lethal.MOLOTOV_COCKTAIL.value,
         Tactical.FLASH_GRENADE.value,
+        Tactical.SNAPSHOT_GRENADE.value,
         Tactical.SHOCK_STICK.value,
         Tactical.STIM.value,
         Tactical.TEAR_GAS.value,
         Tactical.EMD_GRENADE.value,
-        Tactical.SNAPSHOT_GRENADE.value,
     ],
     "FieldUpgrades": [
-        FieldUpgrade.DDOS.value,
-        FieldUpgrade.INFLATABLE_DECOY.value,
-        FieldUpgrade.TROPHY_SYSTEM.value,
-        FieldUpgrade.LOADOUT_DROP.value,
-        FieldUpgrade.TACTICAL_CAMERA.value,
         FieldUpgrade.HEARTBEAT_SENSOR.value,
+        FieldUpgrade.TROPHY_SYSTEM.value,
+        FieldUpgrade.INFLATABLE_DECOY.value,
+        FieldUpgrade.TACTICAL_CAMERA.value,
+        FieldUpgrade.DDOS.value,
+        FieldUpgrade.DEAD_SILENCE.value,
+        FieldUpgrade.LOADOUT_DROP.value,
+        FieldUpgrade.SMOKE_AIRDROP.value,
         FieldUpgrade.SUPPRESSION_MINE.value,
         FieldUpgrade.ANTI_ARMOR_ROUNDS.value,
-        FieldUpgrade.SMOKE_AIRDROP.value,
-        FieldUpgrade.DEAD_SILENCE.value,
     ],
     "Killstreaks": [
+        "BOMB DRONE",
         "CARE PACKAGE",
         "CLUSTER MINE",
-        "WHEELSON",
         "MORTAR STRIKE",
-        "JUGGERNAUT",
-        "ADVANCED UAV",
-        "EMERGENCY AIRDROP",
+        "WHEELSON", 
+        "EMERGENCY AIRDROP",       
         "VTOL JET",
+        "ADVANCED UAV",
         "GUNSHIP",
-        "BOMB DRONE",
         "EMP",
+        "JUGGERNAUT",
         "SWARM"
     ],
 }
